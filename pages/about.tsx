20d4e64de99df12cff31889bc3d75dced93032dc
Cristian <cris18002011@hotmail.com>
2022-06-23 16:05:23 -0500

    Addition of alias with path alias 🎅

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Nabvar from '../components/Navbar'
 
 const About = () => {
   return (
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react'
-import Nabvar from '../components/Navbar'
 
 const Home = () => {
   const [productList, setProductList] = useState<TProduct[]>([])
